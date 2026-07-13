e56ed8ca23f7fd4f975cd6c0f7c4ca7ff958c048
therivernix <therivernix@tommygeusens.com>
2026-07-13 17:26:52 +0200

    adding 1password to image

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,8 +12,15 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+
+sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+
+
+
+
 # this installs a package from fedora repos
-dnf5 install -y tmux firefox firefox-langpacks
+dnf5 install -y tmux firefox firefox-langpacks 1password
 
 # Use a COPR Example:
 #
