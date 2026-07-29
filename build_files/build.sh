ffc6487b79227ab5f94ad018cfa156c1b575307c
therivernix <therivernix@tommygeusens.com>
2026-07-29 12:52:15 +0200

    removing extensions that slow down performance

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,8 +33,9 @@ rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcam
 rm -rf /usr/share/gnome-shell/extensions/logomenu@aryan_k
 rm -rf /usr/share/gnome-shell/extensions/restartto@tiagoporsch.github.io
 rm -rf /usr/share/gnome-shell/extensions/light-style@gnome-shell-extensions.gcampax.github.com
-#rm -rf /usr/share/gnome-shell/extensions/tiling-assistant@leleat-on-github
+rm -rf /usr/share/gnome-shell/extensions/tiling-assistant@leleat-on-github
 rm -rf /usr/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com
+rm -rf /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
 
 #rm -rf /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
 
