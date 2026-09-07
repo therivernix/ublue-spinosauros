081d0d0f01eec9b8cb5de871fcd16ff22678ae6d
therivernix <therivernix@tommygeusens.com>
2026-09-07 19:58:11 +0200

    Enabling Blur My Shell extension.

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,7 +33,7 @@ rm -rf /usr/share/gnome-shell/extensions/places-menu@gnome-shell-extensions.gcam
 rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com
 rm -rf /usr/share/gnome-shell/extensions/logomenu@aryan_k
 rm -rf /usr/share/gnome-shell/extensions/tiling-assistant@leleat-on-github
-rm -rf /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
+# rm -rf /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
 rm -rf /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
 
 # Compile GSettings schemas for GNOME extensions
