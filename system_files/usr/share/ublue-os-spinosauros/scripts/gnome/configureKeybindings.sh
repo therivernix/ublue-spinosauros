4c88f1a5d3c1b8c63d7582e574247531497a0176
therivernix <therivernix@tommygeusens.com>
2026-09-13 14:16:59 +0200

    Updating dconf settings and loading mechanism

---
+++ system_files/usr/share/ublue-os-spinosauros/scripts/gnome/configureKeybindings.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SENTINEL="$HOME/.local/state/spinosauros-configurekeybindings.done"
+
+if [[ -f "$SENTINEL" ]]; then
+    exit 0
+fi
+
+mkdir -p "$(dirname "$SENTINEL")"
+
+touch "$SENTINEL"
+
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < /usr/share/ublue-os-spinosauros/dconf/media-keys.dconf
+dconf load /org/gnome/desktop/wm/keybindings/ < /usr/share/ublue-os-spinosauros/dconf/wm-keybindings.dconf
+dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < /usr/share/ublue-os-spinosauros/dconf/custom-keybindings.conf
