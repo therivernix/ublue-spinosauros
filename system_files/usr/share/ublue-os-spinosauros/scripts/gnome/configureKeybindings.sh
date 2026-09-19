dfd3c42ae2dcd14cc67448739bffd53483dabd97
therivernix <therivernix@tommygeusens.com>
2026-09-19 12:25:05 +0200

    Adding shell keybindings

--- system_files/usr/share/ublue-os-spinosauros/scripts/gnome/configureKeybindings.sh
+++ system_files/usr/share/ublue-os-spinosauros/scripts/gnome/configureKeybindings.sh
@@ -14,3 +14,4 @@ touch "$SENTINEL"
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < /usr/share/ublue-os-spinosauros/dconf/media-keys.dconf
 dconf load /org/gnome/desktop/wm/keybindings/ < /usr/share/ublue-os-spinosauros/dconf/wm-keybindings.dconf
 dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < /usr/share/ublue-os-spinosauros/dconf/custom-keybindings.conf
+dconf load /org/gnome/shell/keybindings/ < /usr/share/ublue-os-spinosauros/dconf/shell-keybindings.dconf
